317106a95c65101616172cd335d06aaf816fa099
paul soscia <paulsoscia@gmail.com>
2023-06-24 10:35:51 -0400

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -8,37 +8,40 @@ class Deck {
     private static final int DeckUpperBound = 6 ;
 
 
-    public static String fCard(String sInput) {
-       Integer int_random2 = 0 ; 
+    public static String fCard(int iRandomCard, int iRandomSuit, int iRandomDeck, String sCard ) {
+       //Integer iRandomSuit = 0 ; 
        String sSuit = "";
        String sFaceCard = ""; 
-       Integer int_random = 0;
-       Integer int_randDeck = 0; 
+       //Integer iRandomCard = 0;
+       //Integer iRandomDeck = 0; 
        String sCardMU = ""; 
-       String sCard = ""; 
+       //String sCard = ""; 
 
-               if (int_random2 == 0) 
+               if (iRandomSuit == 0) 
                   sSuit = "C";
-               if (int_random2 == 1) 
+                  // (char)'\u2663'
+               if (iRandomSuit == 1) 
                   sSuit = (char)'\u2660' +""; //"S";
-               if (int_random2 == 2) 
+               if (iRandomSuit == 2) 
                   sSuit = "H";
-               if (int_random2 == 3) 
+		  // (char)'\u2764'
+               if (iRandomSuit == 3) 
                   sSuit = "D";
+                  // (char)'\u2666'
 
-                sFaceCard = "" + int_random;
-                if (int_random == 0)
+                sFaceCard = "" + iRandomCard;
+                if (iRandomCard == 0)
 			sFaceCard = "J";
-                if (int_random == 1)
+                if (iRandomCard == 1)
 			sFaceCard = "A";
-                if (int_random == 11)
+                if (iRandomCard == 11)
 			sFaceCard = "Q";
-                if (int_random == 12)
+                if (iRandomCard == 12)
 			sFaceCard = "K";
-        	sCard   = sCard + sFaceCard + " " + sSuit + " d=" + int_randDeck + "";
+        	sCard   = sCard + sFaceCard + " " + sSuit + " d=" + iRandomDeck + "";
                 sCardMU = sCard ; 
 
-		return("");
+		return( sCard );
  
     }
 
@@ -60,73 +63,38 @@ for (int j =0 ; j<(52 *DeckUpperBound) ; j++) {
       int upperbound     = 13; // Generating random values from 0 - 12 
        
       // using nextInt()
-      int int_random   = rand.nextInt(upperbound); 
-      int int_random2  = rand2.nextInt(4); 
-      int int_randDeck = randDeck.nextInt(DeckUpperBound); 
+      int iRandomCard   = rand.nextInt(upperbound); 
+      int iRandomSuit  = rand2.nextInt(4); 
+      int iRandomDeck = randDeck.nextInt(DeckUpperBound); 
       String sCard = "";
       String sCardMU = "";
       String sSuit = "";
       String sFaceCard = "";
  
-		if (int_random <= 9 || int_random >= 11) {
+		if (iRandomCard <= 9 || iRandomCard >= 11) {
 			sCard = " " ;
 		}
 
-               if (int_random2 == 0) 
-                  sSuit = "C";
-               if (int_random2 == 1) 
-                  sSuit = (char)'\u2660' + "" ; //"S";
-               if (int_random2 == 2) 
-                  sSuit = "H";
-               if (int_random2 == 3) 
-                  sSuit = "D";
+                sCardMU = fCard( iRandomCard, iRandomSuit,  iRandomDeck, sCard ) ; 
+                sCard = sCardMU ;
 
-                sFaceCard = "" + int_random;
-                if (int_random == 0)
-			sFaceCard = "J";
-                if (int_random == 1)
-			sFaceCard = "A";
-                if (int_random == 11)
-			sFaceCard = "Q";
-                if (int_random == 12)
-			sFaceCard = "K";
-        	sCard   = sCard + sFaceCard + " " + sSuit + " d=" + int_randDeck + "";
-                sCardMU = sCard ; 
-
-/* https://stackoverflow.com/questions/21654581/trying-to-get-a-hand-of-cards-to-display-suit-symbol */
-      /* Collision ? ToDo replace duplicate (2x) code with singular function, called in two locations */
+	/* https://stackoverflow.com/questions/21654581/trying-to-get-a-hand-of-cards-to-display-suit-symbol */
+      	/* Collision ? ToDo replace duplicate (2x) code with singular function, called in two locations */
       while (hashDeckOfCards.contains(sCard)) {
                 System.out.println("duplicate card j=" + j + " sCard=" + sCard);
 
-	        int_random  = rand.nextInt(upperbound); 
-      	        int_random2 = rand2.nextInt(4); 
-                int_randDeck = randDeck.nextInt(DeckUpperBound); 
+	        iRandomCard  = rand.nextInt(upperbound); 
+      	        iRandomSuit = rand2.nextInt(4); 
+                iRandomDeck = randDeck.nextInt(DeckUpperBound); 
 
 		sCard = "";
-		if (int_random <= 9 || int_random >= 11) {
+		if (iRandomCard <= 9 || iRandomCard >= 11) {
 			sCard = " " ;
 		}
 
-               if (int_random2 == 0) 
-                  sSuit = "C";
-               if (int_random2 == 1) 
-                  sSuit = (char)'\u2660' +""; //"S";
-               if (int_random2 == 2) 
-                  sSuit = "H";
-               if (int_random2 == 3) 
-                  sSuit = "D";
 
-                sFaceCard = "" + int_random;
-                if (int_random == 0)
-			sFaceCard = "J";
-                if (int_random == 1)
-			sFaceCard = "A";
-                if (int_random == 11)
-			sFaceCard = "Q";
-                if (int_random == 12)
-			sFaceCard = "K";
-        	sCard   = sCard + sFaceCard + " " + sSuit + " d=" + int_randDeck + "";
-                sCardMU = sCard ; 
+                sCardMU  = fCard( iRandomCard, iRandomSuit,  iRandomDeck, sCard )  ; 
+                sCard = sCardMU ; 
 	}
 
 	deckOfCards.add(sCardMU);
@@ -149,5 +117,3 @@ for (int j =0 ; j<(52 *DeckUpperBound) ; j++) {
 
 
 } /* Class */
-
-//}
